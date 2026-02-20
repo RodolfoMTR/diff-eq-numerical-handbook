e1d6f5a5868af7b5c980a78e11be1e653c2a0b65
rodolfo chuwi <rodolfoturporamos@gmail.com>
2026-02-19 21:47:45 -0500

    modif. de informes

--- scripts/diferencias-finitas/dif_finitas_auto.m
+++ scripts/diferencias-finitas/dif_finitas_auto.m
@@ -1,5 +1,5 @@
 % y''+py''+qy=r(x)
-p=3;q=2;a=0;b=1;n=10;
+p=3;q=2;a=0;b=1;n=6;
 ya=0;yb=1;
 %------------------
 h=(b-a)/n;
@@ -14,8 +14,11 @@ for i=2:(n-2)
         b(i)=h.^2*(i*h);
 end%i
 A(n-1,n-2)=1-3*h/2;A(n-1,n-1)=2*h.^2-2;b(n-1)=(a+(n-1)*h)*h.^2-(1+3*h/2)*yb;
-[A b']
-cc=A\b';cc=[ya cc' yb];
+fprintf('sistema a resolver [A | b]\n')
+disp([A b'])
+cc=A\b';
+fprintf('Solucion del sistema\n')
+disp(cc)
+cc=[ya cc' yb];
 t=a:h:b1;
-plot(t,cc,'LineWidth', 2);  % Cambia el 2 por el grosor que desees
-
+plot(t,cc,'-b'); 
